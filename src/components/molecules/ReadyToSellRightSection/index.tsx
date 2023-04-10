fd47e9066ae0233e9251b284006daaad143ce3b8
Maulana Rheza <103730296+MaulanaRhezaArdiansyah@users.noreply.github.com>
2023-04-10 14:07:50 +0800

    conditional video playing

--- src/components/molecules/ReadyToSellRightSection/index.tsx
+++ src/components/molecules/ReadyToSellRightSection/index.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactPlayer from "react-player";
 
 export const ReadyToSellRightSection: React.FC = () => {
+  const [play, setPlay] = useState(false);
   return (
     <div className="pr-20 relative w-1/2 flex justify-end items-end">
       <div className="w-[500px] h-[400px] relative bg-white/30 backdrop-blur-md rounded-md flex items-center justify-center overflow-hidden">
         <ReactPlayer
-          playing
+          // playIcon
+          onPause={() => {
+            console.log("paused");
+            setPlay(false);
+          }}
+          onPlay={() => {
+            console.log("resume");
+            setPlay(true);
+          }}
+          playing={true}
           controls
           loop
           light={
             <img
-              // src="../../../../public/house-play-video.png"
               src="https://househunter.vercel.app/house-play-video.png"
               alt="thumbnail"
               className="w-full h-full"
@@ -22,18 +31,21 @@ export const ReadyToSellRightSection: React.FC = () => {
           height={"100%"}
         />
       </div>
-      <div className="absolute -bottom-10 left-8 rounded-md">
-        {/* <img src="../../../../public/sofa.png" alt="" /> */}
-        <img src="https://househunter.vercel.app/sofa.png" alt="" />
-      </div>
-      <div className="absolute -bottom-10 left-[340px] rounded-md">
-        {/* <img src="../../../../public/chair.png" alt="" /> */}
-        <img src="https://househunter.vercel.app/chair.png" alt="" />
-      </div>
-      <div className="absolute -bottom-10 right-[90px] rounded-md">
-        {/* <img src="../../../../public/accessories.png" alt="" /> */}
-        <img src="https://househunter.vercel.app/accessories.png" alt="" />
-      </div>
+      {play ? (
+        <></>
+      ) : (
+        <>
+          <div className="absolute -bottom-10 left-8 rounded-md">
+            <img src="https://househunter.vercel.app/sofa.png" alt="" />
+          </div>
+          <div className="absolute -bottom-10 left-[340px] rounded-md">
+            <img src="https://househunter.vercel.app/chair.png" alt="" />
+          </div>
+          <div className="absolute -bottom-10 right-[90px] rounded-md">
+            <img src="https://househunter.vercel.app/accessories.png" alt="" />
+          </div>
+        </>
+      )}
     </div>
   );
 };
